e74a66aba14ec0f0dc570183c84b5b0e92b7083d
root <root@localhost.localdomain>
2020-01-21 15:58:05 +0100

    deuxieme maj

--- dump/sncf_db.sql
+++ dump/sncf_db.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.4
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : lun. 20 jan. 2020 à 13:16
--- Version du serveur :  8.0.18
+-- Généré le : mar. 21 jan. 2020 à 11:57
+-- Version du serveur :  8.0.19
 -- Version de PHP : 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -23,16 +23,26 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-USE sncf_db;
+
 --
 -- Structure de la table `gare`
 --
 
 CREATE TABLE `gare` (
-  `id_gare` int(11) NOT NULL,
+  `id_gare` int NOT NULL,
   `nom_gare` varchar(100) DEFAULT NULL,
-  `id_cp` int(11) DEFAULT NULL
+  `id_cp` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `gare`
+--
+
+INSERT INTO `gare` (`id_gare`, `nom_gare`, `id_cp`) VALUES
+(123, 'gare perrache', 69),
+(456, 'gare montparnasse', 75),
+(789, 'niceville', 6),
+(101112, 'valencegare', 26);
 
 -- --------------------------------------------------------
 
@@ -41,12 +51,22 @@ CREATE TABLE `gare` (
 --
 
 CREATE TABLE `train` (
-  `id_train` int(11) NOT NULL,
-  `id_gare_depart` int(11) DEFAULT NULL,
-  `id_gare_arrivee` int(11) DEFAULT NULL,
+  `id_train` int NOT NULL,
+  `id_gare_depart` int DEFAULT NULL,
+  `id_gare_arrivee` int DEFAULT NULL,
   `heure_depart` datetime DEFAULT NULL,
   `heure_arrivee` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `train`
+--
+
+INSERT INTO `train` (`id_train`, `id_gare_depart`, `id_gare_arrivee`, `heure_depart`, `heure_arrivee`) VALUES
+(1, 123, 456, '2020-01-21 11:42:18', '2020-01-23 11:42:18'),
+(2, 456, 123, '2020-01-27 11:42:18', '2020-01-31 11:42:18'),
+(3, 789, 101112, '2020-01-23 12:45:24', '2020-01-30 17:45:24'),
+(4, 101112, 123, '2020-01-30 12:45:24', '2020-01-31 15:45:24');
 
 -- --------------------------------------------------------
 
@@ -55,9 +75,19 @@ CREATE TABLE `train` (
 --
 
 CREATE TABLE `ville` (
-  `id_cp` int(11) NOT NULL,
+  `id_cp` int NOT NULL,
   `nom_ville` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `ville`
+--
+
+INSERT INTO `ville` (`id_cp`, `nom_ville`) VALUES
+(6, 'nice'),
+(26, 'valence'),
+(69, 'lyon'),
+(75, 'paris');
 
 --
 -- Index pour les tables déchargées
